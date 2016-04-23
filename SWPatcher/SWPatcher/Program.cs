edbef048024715441a683cb82c350636ab2fa5e1
Miyuyami <tenshi.tenshi.angel@gmail.com>
2016-04-23 09:41:19 -0700

    let's try this!

--- SWPatcher/SWPatcher/Program.cs
+++ SWPatcher/SWPatcher/Program.cs
@@ -1,154 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Net;
+using System.Diagnostics;
 
 namespace SWPatcher
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private static string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+        private static string mutexId = string.Format("Global\\{{{0}}}", appGuid);
+        private static Mutex mutex = null;
+
+        private static bool IsUserAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private static bool IsAppAlreadyRunning()
+        {
+            bool createdNew;
+            var allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+            mutex = new Mutex(false, mutexId, out createdNew, securitySettings);
+            return !mutex.WaitOne(TimeSpan.Zero, true);
+        }
+
+        private static bool NewPatcherUpdateAvailable()
+        {
+            Uri patcherGitHubHome = new Uri("https://raw.githubusercontent.com/Miyuyami/SWHQPatcher/master/");
+            string patcherVersionFilePath = "version";
+            WebClient webClient = new WebClient();
+            webClient.BaseAddress = patcherGitHubHome.AbsoluteUri;
+            string patcherVersionFile = webClient.DownloadString(patcherVersionFilePath);
+            string[] lines = patcherVersionFile.Split('\n');
+            Version currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            Version readVersion = new Version(lines[0]);
+            if (currentVersion.CompareTo(readVersion) < 0)
+            {
+                DialogResult newVersionDialog = MessageBox.Show("There is a new patcher version available!\n\nYes - Application will close and redirect you to the patcher website.\nNo - Ignore", "New Patcher Version Available!", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                if (newVersionDialog == DialogResult.Yes)
+                {
+                    Process.Start(lines[1]);
+                    return true;
+                }
+                else
+                {
+                    DialogResult newVersionDialog2 = MessageBox.Show("Are you sure you want to ignore the update?\nIt might cause unknown problems!", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    if (newVersionDialog2 == DialogResult.No)
+                    {
+                        Process.Start(lines[1]);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         [STAThread]
         static void Main()
         {
+            if (!IsUserAdministrator())
+            {
+                MessageBox.Show("You must run this application as administrator.", "Administrator rights", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                return;
+            }
+            if (IsAppAlreadyRunning())
+                return;
+            if (NewPatcherUpdateAvailable())
+                return;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            System.Reflection.Assembly CurrentAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-            System.Runtime.InteropServices.GuidAttribute theObj = (System.Runtime.InteropServices.GuidAttribute)(CurrentAssembly.GetCustomAttributes(typeof(System.Runtime.InteropServices.GuidAttribute), true)[0]);
-            bool instanceCountOne = false;
-            using (System.Threading.Mutex mtex = new System.Threading.Mutex(true, "Global\\" + theObj.Value, out instanceCountOne))
-            {
-                if (instanceCountOne)
-                {
-                    Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-                    Application.ApplicationExit += new EventHandler(MyApplication_OnExit);
-                    Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(MyApplication_ThreadException);
-                    for (short i = 1; i <= 3; i++)
-                    {
-                        try
-                        {
-                            /*i think for some reasons, user internet may get unstable and get stuck on waiting request.
-                             *So we might need TimeOut from HTTP Request....WebClient can but for simple, HTTP serve its purpose ...
-                             */
-                            System.Collections.Generic.List<string> responseArray = new List<string>();
-                            System.Net.HttpWebRequest TheRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create("https://raw.githubusercontent.com/Miyuyami/SWHQPatcher/master/version");
-                            TheRequest.Timeout = 5000;
-                            TheRequest.UserAgent = "SWHQ";
-                            TheRequest.AllowAutoRedirect = true;
-                            TheRequest.Proxy = null;
-                            System.Net.HttpWebResponse TheResponse = (System.Net.HttpWebResponse)TheRequest.GetResponse();
-                            if (TheResponse != null)
-                            {
-
-                                using (System.IO.Stream TheResponseStream = TheResponse.GetResponseStream())
-                                using (System.IO.StreamReader theStreamReader = new System.IO.StreamReader(TheResponseStream))
-                                    while (theStreamReader.EndOfStream == false)
-                                    {
-                                        responseArray.Add(theStreamReader.ReadLine());
-                                    }
-                                TheResponse.Close();
-
-                                System.Version TheVersionObj;
-                                if (System.Version.TryParse(responseArray[0], out TheVersionObj) == false)
-                                    TheVersionObj = new System.Version("0.0.0.1");
-                                System.Version CurrentVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-                                int compareResult = TheVersionObj.CompareTo(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
-                                if (compareResult > 0)
-                                {
-                                    // Ask user to update or skip. I think i should make a Update Changes box for this .... ?
-                                    if (MessageBox.Show("Latest version: " + TheVersionObj.ToString() + "\nCurrent version: " + CurrentVersion.ToString() + "\nDo you want to go to SWHQ ?", "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
-                                    {
-                                        string defaultBrowser = GetDefaultBrowserPath();
-                                        if (string.IsNullOrWhiteSpace(defaultBrowser) == false)
-                                        {
-                                            System.Diagnostics.Process.Start(defaultBrowser, responseArray[1]);
-                                            Application.Exit();
-                                        }
-                                        else
-                                        {
-                                            MessageBox.Show("Unknown Error: Cannot found default browser.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                            Application.Run(new Form1());
-                                        }
-                                    }
-                                    else
-                                    {
-                                        MessageBox.Show("*Insert the skip song*", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                        Application.Run(new Form1());
-                                    }
-                                }
-                                else if (compareResult == 0)
-                                    Application.Run(new Form1());
-                                else
-                                    Application.Run(new Form1());
-                                TheVersionObj = null; // i know this is not necessary, but i think it attract the GC.;
-                                break;
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-
-                        }
-                    }
-                }
-                else
-                    MessageBox.Show("It's running .......");
-                mtex.ReleaseMutex();
-            }
-        }
-
-        static void MyApplication_OnExit(object sender, EventArgs e)
-        {
-
-        }
-
-        static void MyApplication_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
-        {
-
-        }
-        
-        private static string CleanifyBrowserPath(string p)
-        {
-            string[] url = p.Split('"');
-            string clean = url[1];
-            return clean;
-        }
-
-        public static string GetDefaultBrowserPath()
-        {
-            string urlAssociation = @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http";
-            string browserPathKey = @"$BROWSER$\shell\open\command";
-            Microsoft.Win32.RegistryKey userChoiceKey = null;
-            string browserPath = "";
-            try
-            {
-                userChoiceKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(urlAssociation + @"\UserChoice", false);
-                if (userChoiceKey == null)
-                {
-                    var browserKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(@"HTTP\shell\open\command", false);
-                    if (browserKey == null)
-                        browserKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(urlAssociation, false);
-                    var path = CleanifyBrowserPath(browserKey.GetValue(null) as string);
-                    browserKey.Close();
-                    return path;
-                }
-                else
-                {
-                    string progId = (userChoiceKey.GetValue("ProgId").ToString());
-                    userChoiceKey.Close();
-                    string concreteBrowserKey = browserPathKey.Replace("$BROWSER$", progId);
-                    var kp = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(concreteBrowserKey, false);
-                    browserPath = CleanifyBrowserPath(kp.GetValue(null) as string);
-                    kp.Close();
-                    return browserPath;
-                }
-            }
-            catch
-            {
-                return "";
-            }
+            Application.Run(new MainForm());
+            mutex.ReleaseMutex();
         }
     }
 }
---
+++ SWPatcher/SWPatcherOLD/SWPatcher/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SWPatcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            System.Reflection.Assembly CurrentAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Runtime.InteropServices.GuidAttribute theObj = (System.Runtime.InteropServices.GuidAttribute)(CurrentAssembly.GetCustomAttributes(typeof(System.Runtime.InteropServices.GuidAttribute), true)[0]);
+            bool instanceCountOne = false;
+            using (System.Threading.Mutex mtex = new System.Threading.Mutex(true, "Global\\" + theObj.Value, out instanceCountOne))
+            {
+                if (instanceCountOne)
+                {
+                    Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+                    Application.ApplicationExit += new EventHandler(MyApplication_OnExit);
+                    Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(MyApplication_ThreadException);
+                    for (short i = 1; i <= 3; i++)
+                    {
+                        try
+                        {
+                            /*i think for some reasons, user internet may get unstable and get stuck on waiting request.
+                             *So we might need TimeOut from HTTP Request....WebClient can but for simple, HTTP serve its purpose ...
+                             */
+                            System.Collections.Generic.List<string> responseArray = new List<string>();
+                            System.Net.HttpWebRequest TheRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create("https://raw.githubusercontent.com/Miyuyami/SWHQPatcher/master/version");
+                            TheRequest.Timeout = 5000;
+                            TheRequest.UserAgent = "SWHQ";
+                            TheRequest.AllowAutoRedirect = true;
+                            TheRequest.Proxy = null;
+                            System.Net.HttpWebResponse TheResponse = (System.Net.HttpWebResponse)TheRequest.GetResponse();
+                            if (TheResponse != null)
+                            {
+
+                                using (System.IO.Stream TheResponseStream = TheResponse.GetResponseStream())
+                                using (System.IO.StreamReader theStreamReader = new System.IO.StreamReader(TheResponseStream))
+                                    while (theStreamReader.EndOfStream == false)
+                                    {
+                                        responseArray.Add(theStreamReader.ReadLine());
+                                    }
+                                TheResponse.Close();
+
+                                System.Version TheVersionObj;
+                                if (System.Version.TryParse(responseArray[0], out TheVersionObj) == false)
+                                    TheVersionObj = new System.Version("0.0.0.1");
+                                System.Version CurrentVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                                int compareResult = TheVersionObj.CompareTo(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+                                if (compareResult > 0)
+                                {
+                                    // Ask user to update or skip. I think i should make a Update Changes box for this .... ?
+                                    if (MessageBox.Show("Latest version: " + TheVersionObj.ToString() + "\nCurrent version: " + CurrentVersion.ToString() + "\nDo you want to go to SWHQ ?", "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+                                    {
+                                        string defaultBrowser = GetDefaultBrowserPath();
+                                        if (string.IsNullOrWhiteSpace(defaultBrowser) == false)
+                                        {
+                                            System.Diagnostics.Process.Start(defaultBrowser, responseArray[1]);
+                                            Application.Exit();
+                                        }
+                                        else
+                                        {
+                                            MessageBox.Show("Unknown Error: Cannot found default browser.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                            Application.Run(new Form1());
+                                        }
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("*Insert the skip song*", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                        Application.Run(new Form1());
+                                    }
+                                }
+                                else if (compareResult == 0)
+                                    Application.Run(new Form1());
+                                else
+                                    Application.Run(new Form1());
+                                TheVersionObj = null; // i know this is not necessary, but i think it attract the GC.;
+                                break;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("It's running .......");
+                mtex.ReleaseMutex();
+            }
+        }
+
+        static void MyApplication_OnExit(object sender, EventArgs e)
+        {
+
+        }
+
+        static void MyApplication_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+
+        }
+        
+        private static string CleanifyBrowserPath(string p)
+        {
+            string[] url = p.Split('"');
+            string clean = url[1];
+            return clean;
+        }
+
+        public static string GetDefaultBrowserPath()
+        {
+            string urlAssociation = @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http";
+            string browserPathKey = @"$BROWSER$\shell\open\command";
+            Microsoft.Win32.RegistryKey userChoiceKey = null;
+            string browserPath = "";
+            try
+            {
+                userChoiceKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(urlAssociation + @"\UserChoice", false);
+                if (userChoiceKey == null)
+                {
+                    var browserKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(@"HTTP\shell\open\command", false);
+                    if (browserKey == null)
+                        browserKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(urlAssociation, false);
+                    var path = CleanifyBrowserPath(browserKey.GetValue(null) as string);
+                    browserKey.Close();
+                    return path;
+                }
+                else
+                {
+                    string progId = (userChoiceKey.GetValue("ProgId").ToString());
+                    userChoiceKey.Close();
+                    string concreteBrowserKey = browserPathKey.Replace("$BROWSER$", progId);
+                    var kp = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(concreteBrowserKey, false);
+                    browserPath = CleanifyBrowserPath(kp.GetValue(null) as string);
+                    kp.Close();
+                    return browserPath;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}

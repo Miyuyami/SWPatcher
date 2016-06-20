2b861e772302312ab0a9c3df8ed1deffb6366c6a
Dramiel Leayal <whitered12316@gmail.com>
2016-06-21 05:16:30 +0700

    Cleaned up messagebox for debugging purpose.

--- SWPatcher/SWPatcherTest/Properties/AssemblyInfo.cs
+++ SWPatcher/SWPatcherTest/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.12")]
-[assembly: AssemblyFileVersion("2.1.2.12")]
+[assembly: AssemblyVersion("2.1.2.13")]
+[assembly: AssemblyFileVersion("2.1.2.13")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

09122c7355366847c8f7966fef64691206cb5d9e
Miyuyami <18267786+Miyuyami@users.noreply.github.com>
2021-05-21 09:07:51 +0300

    publish 2.14.1.0

--- SWPatcher/Properties/AssemblyInfo.cs
+++ SWPatcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Miyu")]
 [assembly: AssemblyProduct("SW Patcher")]
-[assembly: AssemblyCopyright("Copyright (C) 2016-2020")]
+[assembly: AssemblyCopyright("Copyright (C) 2016-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.14.0.0")]
-[assembly: AssemblyFileVersion("2.14.0.0")]
+[assembly: AssemblyVersion("2.14.1.0")]
+[assembly: AssemblyFileVersion("2.14.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
